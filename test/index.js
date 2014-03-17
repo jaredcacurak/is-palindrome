24f9e2e955ee3e44a46ca00eb0fbb1b18f562cf8
Jared Cacurak <jared.cacurak@gmail.com>
2014-03-17 11:04:15 -0400

    Elaborate on the test names.

--- test/index.js
+++ test/index.js
@@ -2,75 +2,75 @@ var should = require('chai').should(),
     isPalindrome = require('../index');
 
 describe('#isPalindrome', function () {
-    it('jared', function () {
+    it('"jared" is not a palindrome', function () {
         isPalindrome('jared').should.be.false;
     });
 
-    it('an empty string', function () {
+    it('an empty string is a palindrome', function () {
         isPalindrome('').should.be.true;
     });
 
-    it('a single character', function () {
+    it('a single character is a palindrome', function () {
         isPalindrome('a').should.be.true;
     });
 
-    it('two characters', function () {
+    it('two identical characters are a palindrome', function () {
         isPalindrome('aa').should.be.true;
     });
 
-    it('racecar', function () {
+    it('"racecar" is a palindrome', function () {
         isPalindrome('racecar').should.be.true;
     });
 
-    it('Amore Roma', function () {
+    it('"Amore Roma" is a palindrome', function () {
         isPalindrome('Amore Roma').should.be.true;
     });
 
-    it('A man, a plan, a canal: Panama', function () {
+    it('"A man, a plan, a canal: Panama" is a palindrome', function () {
         isPalindrome('A man, a plan, a canal: Panama').should.be.true;
     });
 
-    it("No 'x' in 'Nixon'", function () {
+    it("\"No 'x' in 'Nixon'\" is a palindrome", function () {
         isPalindrome("No 'x' in 'Nixon'").should.be.true;
     });
 
-    it('42', function () {
+    it('42 is not a palindrome', function () {
         isPalindrome(42).should.be.false;
     });
 
-    it('1', function () {
+    it('1 is a palindrome', function () {
         isPalindrome(1).should.be.true;
     });
 
-    it('11', function () {
+    it('11 is a palindrome', function () {
         isPalindrome(11).should.be.true;
     });
 
-    it('121', function () {
+    it('121 is a palindrome', function () {
         isPalindrome(121).should.be.true;
     });
 
-    it('0', function () {
+    it('0 is a palindrome', function () {
         isPalindrome(0).should.be.true;
     });
 
-    it('null', function () {
+    it('null is not a palindrome', function () {
         isPalindrome(null).should.be.false;
     });
 
-    it('undefined', function () {
+    it('undefined is not a palindrome', function () {
         isPalindrome(undefined).should.be.false;
     });
 
-    it('-1', function () {
+    it('-1 is not a palindrome', function () {
         isPalindrome(-1).should.be.false;
     });
 
-    it('!@#$', function () {
+    it('"!@#$" is not a palindrome', function () {
         isPalindrome('!@#$', false).should.be.false;
     });
 
-    it('!@#$#@!', function () {
+    it('"!@#$#@!" is a palindrome', function () {
         isPalindrome('!@#$#@!', false).should.be.true;
     });
 });
