e7e94897cd9ac15cf088925df14aaad6de1d832e
Jared Cacurak <jared.cacurak@gmail.com>
2014-03-19 15:44:01 -0400

    Adhere to the Node.js Style Guide, according to @felixge.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-var gulp = require('gulp'),
-    mocha = require('gulp-mocha');
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
 
 gulp.task('test', function () {
-    return gulp.src(['test/*.js'], { read: false })
-            .pipe(mocha({ reporter: 'spec'}));
+  return gulp.src(['test/*.js'], { read: false })
+    .pipe(mocha({ reporter: 'spec'}));
 });
--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 module.exports = function (value /*, ignoreSpecialCharacters */) {
-    var ignoreSpecialCharacters = arguments[1];
+  if (value === null || value === undefined) return false;
 
-    if (value == null) { return false; }
+  var valueIsString = (typeof value === 'string');
+  var ignoreSpecialCharacters = (arguments[1] === undefined);
+  value = (valueIsString && ignoreSpecialCharacters)
+    ? value.toLowerCase().replace(/[^\w]/gi, '')
+    : value.toString();
 
-    ignoreSpecialCharacters = (ignoreSpecialCharacters != null)
-            ? ignoreSpecialCharacters
-            : true;
-    value = (typeof value === 'string' && ignoreSpecialCharacters)
-            ? value.toLowerCase().replace(/[^\w]/gi, '')
-            : value.toString();
+  function test(candidate) {
+    if (candidate.length < 2) return true;
 
-    function test(candidate) {
-
-        if (candidate.length < 2) { return true; }
-
-        var size = candidate.length - 1,
-            first = candidate[0],
-            last = candidate[size],
-            remaining = candidate.slice(1, size);
-        return (first === last)
-                ? test(remaining)
-                : false;
-    }
-    return test(value);
+    var size = candidate.length - 1;
+    var first = candidate[0];
+    var last = candidate[size];
+    var remaining = candidate.slice(1, size);
+    return (first === last)
+      ? test(remaining)
+      : false;
+  }
+  return test(value);
 }
