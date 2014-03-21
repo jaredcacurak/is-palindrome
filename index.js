1c9750aaa9c8466ec76bda11a8081f90ba7794dc
Jared Cacurak <jared.cacurak@gmail.com>
2014-03-21 16:05:54 -0400

    Use native functions to split the string into an array, reverse the array, then join the reversed array back to a string.

--- index.js
+++ index.js
@@ -8,17 +8,5 @@ module.exports = function (value /*, ignoreSpecialCharacters */) {
   value = (valueIsString && ignoreSpecialCharacters)
     ? value.toLowerCase().replace(/[^\w]/gi, '')
     : value.toString();
-
-  function test(candidate) {
-    if (candidate.length < 2) return true;
-
-    var size = candidate.length - 1;
-    var first = candidate[0];
-    var last = candidate[size];
-    var remaining = candidate.slice(1, size);
-    return (first === last)
-      ? test(remaining)
-      : false;
-  }
-  return test(value);
+  return value === value.split('').reverse().join('');
 }
